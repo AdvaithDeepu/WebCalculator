c597b6c764d96ee6ade63b443b6092d6237d038c
AdvaithDeepu <advaith.deepu2007@gmail.com>
2022-03-17 13:48:50 +0530

    Meta information, Interest calculator, new css stylings and separation of css file

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-document.body.style.backgroundColor = "#00FFEE" //initialize background color
-let num1, num2, color,x,y;
+let num1, num2,x,y,z;
+window.onload = function () {
+    document.getElementById("CI").disabled = true;
+    document.body.style.backgroundColor = "#00FFEE" //initialize background color
+    document.getElementById("year").addEventListener("click", function() {
+       x = 1; 
+    document.getElementById("CI").disabled = false;
+       });
+    document.getElementById("half").addEventListener("click", function() {
+       x = 2;
+       document.getElementById("CI").disabled = false;
+       });
+    document.getElementById("quart").addEventListener("click", function() {
+       x = 4;
+       document.getElementById("CI").disabled = false;
+       });
+}
+
 function addition(){
     num1 = parseInt(document.getElementById("addNum1").value);
     num2 = parseInt(document.getElementById("addNum2").value);
@@ -33,17 +49,13 @@ function CubeRoot(){
     num1 = document.getElementById("rootNum").value;
     document.getElementById("rootResult").innerHTML = "Cube root of "+num1+" is "+Math.cbrt(num1)
 }
-function Lemma(x, y){
-    num1 = document.getElementById("HCF1").value;
-    num2 = document.getElementById("HCF2").value;
-    x = num1;
-    y = num2;
+function Lemma(x,y){
     while (1==1){        
-        rem = x%y;
-        if (rem!=0){
+        z = x%y;
+        if (z!=0){
             x = y;
-            y = rem;
-        }else if (rem ==0){ 
+            y = z;
+        }else if (z==0){ 
             break;
         }
     }
@@ -59,7 +71,21 @@ function LCM(){
     num2 = document.getElementById("HCF2").value;
     document.getElementById("Hresult").innerHTML= "The LCM of "+num1+" and "+num2+" is: "+((num1*num2)/Lemma(num1, num2));;
 }
-function ChangeColor(){ //Mood lighting code
-    let color = document.getElementById("colorPicker").value;
-    document.body.style.backgroundColor = color;
+function ChangeColor(){ //Mood lighting code 
+    document.body.style.backgroundColor = document.getElementById("colorPicker").value;
 }
+function SimpleInterest(){
+    num1 = parseFloat(document.getElementById("principal").value);
+    num2 = parseFloat(document.getElementById("rateOfI").value);
+    x = (document.getElementById("time").value)/12;
+    y = (((num1*num2*x)/100)+num1).toFixed(2)
+    document.getElementById("SResult").innerHTML = "The amount is: "+(y)+" and the Simple Interest is: "+(y-num1).toFixed(2);
+}
+function CompoundInterest(){
+    num1 = parseInt(document.getElementById("principal").value);
+    num2 = parseInt(document.getElementById("rateOfI").value);
+    y = (document.getElementById("time").value)/12;
+    z = num1*(1+((num2/x)/100))**(x*y)
+    document.getElementById("CResult").innerHTML = "The amount is: "+(z.toFixed(2))+" and the Simple Interest is: "+(z-num1).toFixed(2);
+}
+
